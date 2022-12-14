8fff9dda9a020277e9597e94de31fc2a02f9451a
zakariyaahmed123 <118251771+zakariyaahmed123@users.noreply.github.com>
2022-12-14 09:11:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  for (let i = 0; i < word.length; i++) {
+    const j = word.length - 1;
+    if (word[i] !== word[j]) {
+      return false;
+    }
+    return true;
+  }
 }
 
 /* 
